20de9e54a7bf34b89129442d42f7c18ab5f53d1f
EliottBDR <eliott.boudier@outlook.com>
2026-05-22 22:25:41 +0200

    Update player.h

--- player.h
+++ player.h
@@ -1,4 +1,4 @@
-#ifndef PLAYER_H 
+#ifndef PLAYER_H  
 #define PLAYER_H
 
 #include "board.h"
@@ -6,20 +6,20 @@
 #define MAX_NAME 32
 
 // Structure d'un joueur :
-typedef struct {
+typedef struct{
     int  id;                 // 1, 2 ou 3 
     char name[MAX_NAME];
-} Player;
+}Player;
 
 // Structure de l'état global de la partie : (C'est cette structure qui est sauvegardée/restaurée.)
-typedef struct {
-    Board   board;
-    Player  players[MAX_PLAYERS];
-    int     nb_players;
-    int     current_idx;    // index dans players[] du joueur actif 
-    int     zone_size;      // taille de zone tirée pour ce tour 
-    int     turn;           // numéro du tour 
-} GameState;
+typedef struct{
+    Board board;
+    Player players[MAX_PLAYERS];
+    int nb_players;
+    int current_idx;    // index dans players[] du joueur actif 
+    int zone_size;      // taille de zone tirée pour ce tour 
+    int turn;           // numéro du tour 
+}GameState;
 
 // Initialise les joueurs (saisie des noms ou noms par défaut) :
 void player_init_all(GameState *gs, int nb_players);
