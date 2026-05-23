6d6959b09e1a259f5ea841d3f7e9d70b01940f65
elsrmsr-art <elsrmsr@gmail.com>
2026-05-23 22:56:15 +0200

    Update save.h

--- save.h
+++ save.h
@@ -1,5 +1,10 @@
 #ifndef SAVE_H 
 #define SAVE_H
+/*
+ * Nom du fichier utilisé pour la sauvegarde.
+ * Toutes les données du jeu seront écrites
+ * dans ce fichier puis relues plus tard.
+ */
 #define SAVE_FILE "sauvegarde.bin"
 #include "player.h"
 int save_game(GameState *gs);
