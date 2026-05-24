ec80c49ec8db5ab6d9b4d648ddd87f882e811c16
elsrmsr-art <elsrmsr@gmail.com>
2026-05-24 17:19:13 +0200

    Update save.c

--- save.c
+++ save.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * Sauvegarde l’état actuel du jeu dans un fichier avec les étapes suivantes
+/*Sauvegarde l’état actuel du jeu dans un fichier avec les étapes suivantes
  * Étapes :
- *   1) On ouvre le fichier en mode écriture binaire.
- *   2) On écrit toute la structure GameState dedans.
- *   3) On ferme le fichier.
+ *   1) On ouvre le fichier en mode écriture binaire
+ *   2) On écrit toute la structure GameState dedans
+ *   3) On ferme le fichier
  * Si le fichier ne peut pas être ouvert : retourne -1
  * Sinon : retourne 0
  */
@@ -21,19 +20,19 @@ int save_game(GameState *gs){
     if (f == NULL){
         return -1;
     }
-    //on ecrit toute la structure GameState dans le fichier binaire pour sauvegarder.
+    //on ecrit toute la structure GameState dans le fichier binaire pour sauvegarder
     fwrite(gs, sizeof(GameState),1,f);
     fclose(f);
     printf("partie sauvegardee \n");
     return 0;
 }
 
-/*Recharge une partie sauvegardée depuis le fichier.
+/*Recharge une partie sauvegardée depuis le fichier
  * Étapes :
- *   1) On ouvre le fichier en lecture binaire.
+ *   1) On ouvre le fichier en lecture binaire
  *   2) On lit les données sauvegardées.
- *   3) On remet ces données dans GameState.
- *   4) On ferme le fichier.
+ *   3) On remet ces données dans GameState
+ *   4) On ferme le fichier
  * Si aucune sauvegarde n’existe : retourne -1
  * Sinon : retourne 0
  */
